4660f204069b69ef167f1d3e213adc6e4435977f
Nick Miller <miller.nick.c@gmail.com>
2024-05-24 12:04:10 -0700

    Update spacing

--- ssh-instance.sh
+++ ssh-instance.sh
@@ -37,7 +37,6 @@ echo "Identity file: $PRIVATE_KEY_PATH"
 # Append config to .ssh/config
 cat << EOF >> ~/.ssh/config
 
-
 Host $INSTANCE_NAME
     HostName $PUBLIC_IP
     IdentityFile $PRIVATE_KEY_PATH
